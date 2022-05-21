010809939b107664f4db0ecfcf540fcc426e5c85
justsaykk <kaikein.woo@gmail.com>
2022-05-21 14:35:31 +0800

    deleted async

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,34 +8,34 @@ const saltRounds = 10;
 const userController = express.Router();
 
 // Generic Get Route
-user.get("/", async (req, res) => {
+user.get("/", (req, res) => {
   res.send({ msg: "Welcome to the user get route" });
 });
 
-// Register new user
-user.post("/register", async (req, res) => {
-  console.log(req.body);
-  const { name, email, password } = req.body;
-  try {
-    // Finding if email is inside database
-    const foundUser = await prisma.User.findUnique({
-      where: {
-        email: email,
-      },
-    });
-    // Create user
-    if (!foundUser) {
-      await prisma.User.create({
-        data: {
-          name: name,
-          email: email,
-          password: bcrypt.hashSync(password, saltRounds),
-        },
-      });
-    }
-  } catch (error) {
-    res.status(404).send({ msg: error });
-  }
-});
+// // Register new user
+// user.post("/register", async (req, res) => {
+//   console.log(req.body);
+//   const { name, email, password } = req.body;
+//   try {
+//     // Finding if email is inside database
+//     const foundUser = await prisma.User.findUnique({
+//       where: {
+//         email: email,
+//       },
+//     });
+//     // Create user
+//     if (!foundUser) {
+//       await prisma.User.create({
+//         data: {
+//           name: name,
+//           email: email,
+//           password: bcrypt.hashSync(password, saltRounds),
+//         },
+//       });
+//     }
+//   } catch (error) {
+//     res.status(404).send({ msg: error });
+//   }
+// });
 
 module.exports = userController;
