42516da1c9c83829093e359d7f1112babae74f53
justsaykk <kaikein.woo@gmail.com>
2022-05-21 14:39:15 +0800

    deleted extra express.route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,41 +1,40 @@
 // Dependencies
 const bcrypt = require("bcrypt");
 const express = require("express");
-const user = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const saltRounds = 10;
 const userController = express.Router();
 
 // Generic Get Route
-user.get("/", (req, res) => {
+userController.get("/", (req, res) => {
   res.send({ msg: "Welcome to the user get route" });
 });
 
-// // Register new user
-// user.post("/register", async (req, res) => {
-//   console.log(req.body);
-//   const { name, email, password } = req.body;
-//   try {
-//     // Finding if email is inside database
-//     const foundUser = await prisma.User.findUnique({
-//       where: {
-//         email: email,
-//       },
-//     });
-//     // Create user
-//     if (!foundUser) {
-//       await prisma.User.create({
-//         data: {
-//           name: name,
-//           email: email,
-//           password: bcrypt.hashSync(password, saltRounds),
-//         },
-//       });
-//     }
-//   } catch (error) {
-//     res.status(404).send({ msg: error });
-//   }
-// });
+// Register new user
+userController.post("/register", async (req, res) => {
+  console.log(req.body);
+  const { name, email, password } = req.body;
+  try {
+    // Finding if email is inside database
+    const foundUser = await prisma.User.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    // Create user
+    if (!foundUser) {
+      await prisma.User.create({
+        data: {
+          name: name,
+          email: email,
+          password: bcrypt.hashSync(password, saltRounds),
+        },
+      });
+    }
+  } catch (error) {
+    res.status(404).send({ msg: error });
+  }
+});
 
 module.exports = userController;
