eda5ade16322c254b095fe0231594df287d8f295
justsaykk <kaikein.woo@gmail.com>
2022-05-21 17:31:41 +0800

    finished create user function

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 // Dependencies
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const cors = require("cors");
 const userController = require("./controllers/userController");
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
 // Routes
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:2000",
+      "http://localhost:4000",
+      "http://capstone-frontend-nu.vercel.app",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 app.use("/api/user", userController);
 
 // Listener
