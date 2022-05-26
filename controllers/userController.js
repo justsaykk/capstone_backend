dffdff5cec15c072c02773df0f029f2c291a1204
justsaykk <kaikein.woo@gmail.com>
2022-05-26 22:24:04 +0800

    Added user seed route and successfully seeded users

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,15 +6,46 @@ const prisma = new PrismaClient();
 const saltRounds = 10;
 const userController = express.Router();
 
+// Seed Data
+const userSeeds = [
+  {
+    name: "user01",
+    email: "user01@user.com",
+    password: bcrypt.hashSync("123456", saltRounds),
+  },
+  {
+    name: "user02",
+    email: "user02@user.com",
+    password: bcrypt.hashSync("123456", saltRounds),
+  },
+  {
+    name: "user03",
+    email: "user03@user.com",
+    password: bcrypt.hashSync("123456", saltRounds),
+  },
+];
+
 // Generic Get Route
 userController.get("/", (req, res) => {
   res.send({ msg: "Welcome to the user get route" });
 });
 
+// Seeed Route
+userController.get("/seed", async (req, res) => {
+  try {
+    await prisma.User.deleteMany({ where: {} });
+    await prisma.User.createMany({
+      data: userSeeds,
+    });
+    res.status(200).send({ msg: "users seeded" });
+  } catch (error) {
+    res.status(404).send({ msg: error });
+  }
+});
+
 // Register new user
 userController.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-
   try {
     const foundUser = await prisma.User.findUnique({
       where: {
@@ -25,7 +56,7 @@ userController.post("/register", async (req, res) => {
     if (foundUser) {
       return res.status(400).send({ msg: "user exists" });
     } else {
-      const newUser = await prisma.User.create({
+      await prisma.User.create({
         data: {
           name: name,
           email: email,
